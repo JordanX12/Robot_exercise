47ef8962e6222501f3d949c9fad85d89b35fb8d2
Jordan <jordiperez_12@hotmail.com>
2017-06-22 15:06:18 +0200

    Create api.c
    Source code for the api

---
+++ api.c
@@ -0,0 +1,55 @@
+#include "api.h"
+
+void RobotTask(uint8_t u8Command)
+{
+
+}
+
+void RobotStop (void)
+{
+
+}
+
+void TimerInit(void)
+{
+
+}
+
+
+
+uint32_t TimetGetus(void)
+{
+    return 0;
+}
+
+void SPIInit(void)
+{
+
+}
+
+void SPIEepromEnable(void)
+{
+
+}
+
+void SPIEepromDisable(void)
+{
+
+}
+
+void SPISensorEnable(void)
+{
+
+}
+
+void SPISensorDisable(void)
+{
+
+}
+
+uint8_t SPIRead(void)
+{
+    return 0;
+}
+
+
